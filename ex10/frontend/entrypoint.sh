56e00a49abcd449eded7bba4ec812a421ff4f134
coderyu5523 <coderyu5523@naver.com>
2025-11-08 14:34:41 +0900

    ex10

--- ex10/frontend/entrypoint.sh
+++ ex10/frontend/entrypoint.sh
@@ -8,4 +8,8 @@ git clone --depth=1 https://github.com/metacoding-10-linux-docker/frontend-serve
 cp -r /tmp/frontend-server/* /usr/share/nginx/html/
 cp /tmp/frontend-server/conf/nginx-ex09.conf /etc/nginx/nginx.conf
 
+# Kubernetes 환경에서는 backend1, backend2 대신 backend 서비스 이름 사용
+sed -i 's/backend1:8080/backend:8080/g' /etc/nginx/nginx.conf
+sed -i 's/backend2:8080/backend:8080/g' /etc/nginx/nginx.conf
+
 exec nginx -g "daemon off;"
